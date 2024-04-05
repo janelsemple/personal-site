61c6f844c73bb053a9e0f0b49ef6306dd5974eb7
Janel Semple <janelsemple@gmail.com>
2024-04-04 21:58:13 -0600

    Fixed deployment bugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,38 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
+  // Move pathPrefix to the top-level of the configuration object
+  pathPrefix: "/personal-site",
   siteMetadata: {
-    title: `Janel Semple`,
-    description: `This is a personal website for Janel Semple, come say hello!`,
-    author: `@janelsemple`,
-    pathPrefix: `/personal-site`,
+    title: "Janel Semple",
+    description: "This is a personal website for Janel Semple, come say hello!",
+    author: "@janelsemple",
   },
   plugins: [
-    // First plugin: gatsby-plugin-manifest
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        start_url: `/`,
-        background_color: `#be7730`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
+        start_url: "/",
+        background_color: "#be7730",
+        display: "minimal-ui",
+        icon: "src/images/favicon-32x32.png", // This path is relative to the root of the site.
       },
     },
-    // Second plugin: gatsby-plugin-google-fonts
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: "gatsby-plugin-google-fonts",
       options: {
         fonts: [
-          `Poppins:400,400i,700, 100`, // Normal 400, Italic 400, Bold 700 for Poppins
-          `Roboto Mono:700` // Bold 700 for Roboto Mono
+          "Poppins:400,400i,700,700i", // Corrected the spacing and added 700i for Poppins
+          "Roboto Mono:700", // Bold 700 for Roboto Mono
         ],
-        display: 'swap',
+        display: "swap",
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images/`,
       },
-    }
+    },
+    // Add any other plugins here
   ],
-}
+};
